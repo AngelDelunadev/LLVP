c357e2f3e69892405e9b6fdb3ee3a67c9edd907e
Angel Deluna <angelluna2016@gmail.com>
2021-06-16 14:09:57 -0400

    added prof check to proficiencies router

--- routes/proficiencies.js
+++ routes/proficiencies.js
@@ -15,13 +15,22 @@ router.get('/:languageId', async(req,res) => {
 })
 
 router.post('/create', async (req,res) => {
-    const newProf = await models.Proficiencies.create({
-        UserId: req.body.UserId,
-        LanguageId: req.body.LanguageId,
-        proficiencyLvl: req.body.profLvl
-    })
+    if( !(req.body.profLvl< 1) && !(req.body.profLvl > 10 ))
+    {
+        const newProf = await models.Proficiencies.create({
+            UserId: req.body.UserId,
+            LanguageId: req.body.LanguageId,
+            proficiencyLvl: req.body.profLvl
+        })
+    
+        return res.status(201).json(newProf)
 
-    return res.status(201).json(newProf)
+    }
+    else{
+        return res.status(403).json({
+            error: "profLvl must between 1 and 10"
+        })
+    }
 })
 
 router.patch('/update' , async (req,res) => {
